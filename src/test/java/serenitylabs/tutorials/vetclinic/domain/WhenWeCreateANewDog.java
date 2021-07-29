0d151e182948c0436937c6a70b524200b6074892
mosesarn <mosesarnoldk9@gmail.com>
2021-07-29 19:23:26 -0400

    done when we create a dog assignment

--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
@@ -15,19 +15,21 @@ public class WhenWeCreateANewDog {
     @Test
     public void a_new_dog_should_have_a_name_and_a_breed_and_a_colour() throws Exception {
         Dog fido = Dog.called("Fido").ofBreed("Labrador").andOfColour("Black");
-
-        Assert.assertEquals("Fido",fido.getName());
-        Assert.assertEquals("Labrador", fido.getBreed());
-        Assert.assertEquals("Black", fido.getColour());
+        assertThat(fido.getName(),equalTo("Fido"));
+        assertThat(fido.getBreed(),equalTo("Labrador"));
+        assertThat(fido.getColour(),contains("Black"));
+//        Assert.assertEquals("Fido",fido.getName());
+//        Assert.assertEquals("Labrador", fido.getBreed());
+//        Assert.assertEquals("[Black]", fido.getColour().toString());
     }
     @Test
     public void a_dog_should_be_printed_in_a_redable_form(){
         Dog fido = Dog.called("Fido").ofBreed("Labrador").andOfColour("Black");
-        assertThat(fido.toString(),is(equalTo("Fido the black labrador")));
+        assertThat(fido.toString(),is(equalTo("Fido the [black] labrador")));
         assertThat(fido.toString(),startsWith("Fido"));
         assertThat(fido.toString(),endsWith("labrador"));
         assertThat(fido.toString(),containsString("black"));
-        assertThat(fido.toString(),is(equalToIgnoringCase("Fido the Black labrador")));
+        assertThat(fido.toString(),is(equalToIgnoringCase("Fido the [black] labrador")));
 
     }
     @Test
@@ -35,7 +37,7 @@ public class WhenWeCreateANewDog {
         Dog fido = Dog.called("Fido").ofBreed("Labrador").andOfColour("Black","White");
         assertThat(fido.getColour(),contains("Black","White"));
         assertThat(fido.getColour(),hasItem("White"));
-        assertThat(fido.getColour(),not(hasItem("Red"));
+        assertThat(fido.getColour(),not(hasItem("Red")));
 
     }
 
